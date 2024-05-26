bee541daa392358abd17b9eeaa780465ee8999bf
Sumanth <sumanthmylar@sumanths-air.notrust.clients.wireless.pfw.edu>
2024-05-26 16:34:58 -0400

    Decision Tree Classifier Implementation

--- Regression/Decision_Tree_Regression.py
+++ Regression/Decision_Tree_Regression.py
@@ -1,32 +1,81 @@
-#Model to predict previous salary of an interview candidate
-#Dataset = Position_Salaries.csv
+#Predict if a customer buys a car or not
 
-#Import libraries
+#Importing libraries
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-#Importing the dataset
-dataset = pd.read_csv('Position_Salaries.csv')
-X = dataset.iloc[:,1:-1].values
+
+#Importing dataset
+dataset = pd.read_csv('Classification/Social_Network_Ads.csv')
+X = dataset.iloc[:,:-1].values
 y = dataset.iloc[:,-1].values
 
-# Training decision tree regression model
-from sklearn.tree import DecisionTreeRegressor
-model = DecisionTreeRegressor(random_state=0)
-model.fit(X,y)
+#Spliting the dataset into train and test
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.25,random_state=0)
+
+#Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+#Training Logistic regression model
+from sklearn.tree import DecisionTreeClassifier
+model = DecisionTreeClassifier(criterion='entropy')
+model.fit(X_train,y_train)
 
 
 #Prediction
-y_pred = model.predict([[6.5]])
+y_pred = model.predict(sc.transform([[30,87000]]))
 # print(y_pred)
 
-#Visualization
-X_grid = np.arange(min(X),max(X),0.1)
-X_grid = X_grid.reshape((len(X_grid),1))
-plt.scatter(X,y,color='red')
-plt.plot(X_grid,model.predict(X_grid),color = 'blue')
-plt.title('Truth of Bluff (Decision Tree Regression)')
-plt.xlabel('Position Level')
-plt.ylabel('Salary')
+#Prediction on test set
+y_pred = model.predict(X_test)
+np.set_printoptions(precision=2)
+# print(np.concatenate((y_pred.reshape(len(y_pred),1),y_test.reshape(len(y_test),1)),1))
+
+
+#Confusion Matrix
+from sklearn.metrics import confusion_matrix,accuracy_score
+conf_matrix = confusion_matrix(y_test,y_pred)
+
+# print(conf_matrix)
+print(accuracy_score(y_test,y_pred))
+
+
+
+#Visualization Training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = sc.inverse_transform(X_train), y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 0.25),
+                     np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 0.25))
+plt.contourf(X1, X2, model.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Decision Tree Regression (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = sc.inverse_transform(X_test), y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 0.25),
+                     np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 0.25))
+plt.contourf(X1, X2, model.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Decision Tree Regression (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
 plt.show()
